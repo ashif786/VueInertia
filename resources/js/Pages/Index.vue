c5d5bed51eba124ce66f6a4543e975a164005791
mohamed ashif <mohamedashif786@gmail.com>
2022-04-19 14:41:13 +0400

    1st commit

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,32 @@
+<template>
+<div class='container mx-auto w-1/2 bg-blue-200 p-4 rounded mt-4'>  
+ <table class="table w-full">
+            <thead>
+                <tr class='border'>
+                    <th class='border'>Id#</th>
+                    <th class='border'>Name</th>
+                    <th class='border'>Email</th>
+                     
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in posts" :key="user.id">
+                    <td  class='border'>{{user.id}} </td>
+                    <td  class='border'>{{user.name}} </td>
+                    <td  class='border'>{{user.email}} </td>
+                   
+                     
+                </tr>
+            </tbody>
+        </table>
+</div>
+</template>
+
+<script>
+export default {
+    props:
+    {
+        posts:Object
+    }
+}
+</script>
